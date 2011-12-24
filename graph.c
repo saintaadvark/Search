c07fbaa50775d890a753f6710686ed652b5a26e4
bar08@live.com <prashanth@ubuntu.ubuntu-domain>
2011-12-24 01:59:43 -0800

    updated graph algorithm

--- graph.c
+++ graph.c
@@ -1,116 +1,90 @@
-#include "graph.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include "graph.h"
+#include "graph_book.h"
 
-// Processes all vertices as follows: 
-// 1. Pop current vertex from FIFO queue
-// 2. Add all its unvisited edge vertices to queue
-// 3. Mark as visited
-// 4. Goto 1. or return if theres nothing left in queue
-void bfs(graph* agraph)
+
+// Not that trivial, just return for today
+void build_word_list(vertex* vert)
 {
-	edge* current;
-	edge* temp = NULL;
-	int i=0;
-	
-	for (current = dequeue(); current != NULL; current = dequeue()) {
-		printf("\n dequeued %d: enqueueing",current->eid);
-		temp = current->next;
-		while(temp != NULL) {
-			if (!(visited[temp->eid])) {
-				visited[temp->eid] = 1;
-				enqueue(&agraph->edges[temp->eid]);
-				printf("%d,",temp->eid);
+	return;
+/*
+	char* string = vert->acro->meaning;
+	int slen = strlen(string),curr=0,start=0;
+	while(curr<slen) {
+		if (string[curr] == ' ') {
+			string[curr] = '\0';
+			if (!fillercheck(sum)) {
+				
 			}
-			temp = temp->next;
+			sum = 0;
 		}
-		visited[current->eid] = 1;
+		sum += (short) string[curr];
 	}
+	
+	return;
+*/
 }
 
-// Recursively visits children until all
-// children of first node have been visited.
-// 1. Visit child recursively
-// 2. get next child of last visited node
-// 3. Do 1.
-// 3. if no children or child already visited, return
-void dfs(graph* agraph, edge* node)
+// function add_vertex: 
+//	The adjacency list has 2 parts:
+//	1. An array of pointers to each vertex
+//	2. A list of edges within each vertex
+// 	This function adds a vertex to the first.
+// return: void	    	                      
+void add_vertex(char* acro, char* meaning)
 {
-	edge* temp;
-	if (node->fvisited || (node->next == NULL))
-		return;
-
-	node->fvisited = true;
-	temp = node->next;
-	while(temp != NULL) {
-		dfs(agraph, &(agraph->edges[temp->eid]));	
-		temp = temp->next;
-	}
-}
-
-// Each vertex has a list of all other vertices it is
-// connected to. Since graph is bidirectional each 
-// vertex is duplicated for every edge it has through 
-// a recursive call.
-void add_edge(int sedge, int dedge, graph* agraph, bool bidi)
-{
-	edge* destedge = (edge*)malloc(sizeof(edge));
-	destedge->eid = dedge;
-	destedge->fvisited = false;
-	destedge->next = NULL;	
+	vertex* vert;
+	int i=0;
+	init(0, (char**)&vert, INIT_VERTEX);
+	vert->acro->acro = (char*)malloc(sizeof(acro));
+	vert->acro->meaning = (char*)malloc(sizeof(meaning));
+	strcpy(vert->acro->acro, acro);
+	strcpy(vert->acro->meaning, meaning);
 	
-	agraph->edges[sedge].eid = sedge;
-	destedge->next = agraph->edges[sedge].next;
-	agraph->edges[sedge].next = destedge;
-	
-	if (bidi)
-		add_edge(dedge, sedge, agraph, false);
-	
-	return;	
-}
+	build_word_list(vert);
 
-// Initialize an array of vertices. For
-// each member of array add edges.
-graph* build_graph(int* nodes, int* edges)
-{
-	graph* agraph = (graph*)malloc(sizeof(graph));
-	int i=0, a, b;
-	for (i=1; i<=MAX_NODES; i++) {
-		agraph->edges[i].eid = i;
-		agraph->edges[i].fvisited = false;
-		agraph->edges[i].next = NULL;
-	}
+	if (USE_HASHMAP)  
+		push(vert);
+	else
+		Insert(&root, vert);
 
-	for (i=0; i<MAX_EDGES; i = i+2) {
-		a = edges[i];
-		b = edges[i+1];
-		add_edge(a, b, agraph, true);
-	}
-	return agraph;
 }
 
 int main()
 {
-	int nodes[MAX_NODES] = {1,2,3,4,5,6};
-	int edges[MAX_EDGES] = {1,6,1,5,1,2,2,3,2,5,5,4,3,4};
-	graph *agraph;
-	edge *temp;
-	int i=0;
-	agraph = build_graph(nodes, edges);
+	graph agraph;
+	int i, nline, nacro;
+	vertex** vert;
+	char acro[100], meaning[100], chbang;
+	FILE *fp;
+	
+	init(0, (char**)(&agraph), INIT_GRAPH);
+	init(MAX_VERTICES, (char**)vstack, INIT_STACK);
+	init_filler_list();
+	root = NULL;
 
-	for (i=1; i<=MAX_NODES; i++) {
-		printf("\n %d:", agraph->edges[i].eid);
-		temp = agraph->edges[i].next;
-		while(temp != NULL) {
-			printf("%d,",temp->eid);
-			temp = temp->next;
+	fp=fopen("internetslang2", "rw");
+	for(nline=0; fgets(acro, 100, fp); nline++) {
+		if (is_upper(acro[1]) == TRUE) {
+			fgets(meaning, 100, fp);
+			add_vertex(acro, meaning);
 		}
-			
- 	}
+	}
+	fclose(fp);
 
-	init_queue();
-	enqueue(&(agraph->edges[1]));	
-	//bfs(agraph);
-	dfs(agraph, &(agraph->edges[1]));
+	while (1) {
+		scanf("%s",acro);
+		printf("%s\n",acro);
+		vert = Search(&root, acro);
+		if (vert)
+			printf("======%s\n",((vertex*)(*vert))->acro->meaning);
+	}
+	
+	if (DBG)
+		Traverse(root);
+	
 	return 0;
 }
--- graph.h
+++ graph.h
@@ -1,57 +1,48 @@
 #include <stdio.h>
-#define MAX_NODES 6
-#define MAX_EDGES 14
+#include <malloc.h>
+#define MAX_VERTICES 500
+#define MAX_EDGES 1000
+#define FILLER_WORDS 500
+#define INIT_STACK 0X00000001
+#define INIT_QUEUE 0x00000002
+#define INIT_GRAPH 0x00000004
+#define INIT_VERTEX 0x00000008
+#define INIT_ALLOCATE 0x00000010
+#define TRUE 1
+#define FALSE 0
+#define USE_HASHMAP 0	// Use a tree or hashmap for adjacency list?
+#define DBG 0
 
-typedef struct edge {
-	bool fvisited;	// useless flag for bfs
-	int eid;	
-	edge* next;	
-} edge;
+// Given a pointer to a vertex in the tree lookup representation 
+// get the address of its left and right vertices
+#define GET_LEFT_REF(parentNode)((vertex**)(&((*parentNode)->left)))  
+#define GET_RIGHT_REF(parentNode)((vertex**)(&(*parentNode)->right)) 
+
+// Caller ensures that parentNode is not NULL
+#define GET_LEFT(parentNode)((vertex*)((*parentNode)->left))
+#define GET_RIGHT(parentNode)((vertex*)((*parentNode)->right))
+
+typedef struct acronym {
+	char* acro;
+	char* meaning;
+	char* wordlist;
+} acronym;
+
+typedef struct vertex {
+	struct acronym* acro;
+	struct vertex* next;
+	struct vertex* left;
+	struct vertex* right;
+} vertex;
 
 typedef struct graph {
-	edge edges[(MAX_NODES+1)];
-	int parents[(MAX_NODES+1)];
-	int nnodes;
+	vertex** vstack;	// Pointer to global stack of vertices 
+	int* vparents;		// Array of parents of corresponding edges
+	int nvertices;		// Size of graph
 } graph; 
 
-edge* queue[MAX_EDGES];		// Unprocessed bfs nodes
-int visited[MAX_NODES+1];	// If node has been visited dont add to queue
-int qstart, qend;		
-
-void init_queue()
-{
-	int i=0;
-	for(i=0; i<MAX_EDGES; i++)
-		queue[i] = NULL;
-	for (i=0; i<MAX_EDGES+1;i++) {
-		visited[i] = 0;
- 	}
-
-	qstart = 0;
-	qend = 0;
-}
-
-// Dequeue FIFO, only time start = end is when
-// nothing has been enqueued yet.
-edge* dequeue()
-{
-	edge* edge;
-	if (qstart == qend) {
-		return NULL;	
-	} else {
-		if (qend > qstart) {
-			edge = queue[qstart];
-			queue[qstart] = NULL;
-			qstart++;
-			return edge;
-		} else {
-			printf("underflow!\n");
-		}
-	}
-}
-
-void enqueue(edge* edge)
-{
-	queue[qend] = edge;
-	qend++;
-}
+vertex** vstack;// the hashmap, NULL if tree representation
+int stacktop;	// top of stack in hashmap representation
+vertex *root;	// root of tree, NULL if hashmap representation
+int fillerlist[FILLER_WORDS];
+int fillermax;
